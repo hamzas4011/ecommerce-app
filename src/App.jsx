8428164a320709b5c57d96e36cf80b89027eb878
Hamza Simsek <hamzas4011@hotmail.com>
2025-01-18 23:31:03 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 
 function App() {
     return (
         <Router>
-            <div>
                 <Navbar />
                 <Routes>
-                <Route path="/" element={<Home />} />
+                   <Route path="/" element={<Home />} />
                 </Routes>
                 <Footer />
-            </div>
         </Router>
     );
 }
