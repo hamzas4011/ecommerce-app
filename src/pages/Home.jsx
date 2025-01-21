cb5411b776ecc9f213913b8a9caeb6b17859ecf1
Hamza Simsek <hamzas4011@hotmail.com>
2025-01-21 21:46:05 +0100

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,12 @@ import '../styles/Home.css';
 function Home() {
     return (
         <div className="home">
-            <h1 className="home-title">Welcome to the Home Page</h1>
+            <h1 className="home-title">Welcome to TechNova</h1>
             <img 
                 src="/images/tech-gadget.jpg" 
                 alt="Tech gadget" 
                 className="home-image" 
             />
-            <p className="home-content">
-                This is where your content goes!
-            </p>
         </div>
     );
 }
