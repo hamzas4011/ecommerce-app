4ba67571a7180608ede6a032cc97d7e5d2136273
Hamza Simsek <hamzas4011@hotmail.com>
2025-01-24 21:52:33 +0100

    home fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,34 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1 className="home-title">WELCOME TO TECHNOVA</h1>
+            <p className="home-description">
+                Discover the latest and greatest in technology. From smart devices 
+                to everyday essentials, TechNova has everything you need to stay ahead.
+            </p>
+            <Link to="/menu" className="home-shop-now">Shop Now</Link>
+            <img 
+                src="/images/tech-gadget.jpg" 
+                alt="Tech gadget" 
+                className="home-image" 
+            />
+        </div>
+    );
+}
+
+export default Home;
+
+
+
+
+
+
+/*
 import React from 'react';
 import '../styles/Home.css';
 
@@ -15,3 +46,5 @@ function Home() {
 }
 
 export default Home;
+
+*/
