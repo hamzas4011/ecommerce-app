c845285bf9b90e5faddf533a6cf93bb2c54f4386
Hamza Simsek <hamzas4011@hotmail.com>
2025-03-04 23:56:20 +0100

    footer fix for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,23 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <div className="footer-about">
-                <h3 className="footer-title">About TechNova</h3>
-                <p className="footer-text">
-                    TechNova, founded in 2025, is your trusted online <br/> store for the latest tech gadgets and innovations.
-                </p>
+            <div className="footer-container">
+                <div className="footer-about">
+                    <h3 className="footer-title">About TechNova</h3>
+                    <p className="footer-text">
+                        TechNova, founded in 2025, is your trusted online store <br/>
+                        for the latest tech gadgets and innovations.
+                    </p>
+                </div>
+                <div className="footer-contact">
+                    <h3 className="footer-title">Contact Us</h3>
+                    <p className="footer-text">📩 support@technova.com</p>
+                    <p className="footer-text">📞 123-4567</p>
+                    <p className="footer-text">🏠 123 Bergen, Norway</p>
+                </div>
             </div>
-            <div className="footer-contact">
-                <h3 className="footer-title">Contact Us</h3>
-                <p className="footer-text">📩 support@technova.com</p>
-                <p className="footer-text">📞 123-4567</p>
-                <p className="footer-text">🏠 123 Bergen, Norway</p>
+            <div className="footer-bottom">
+                <p className="footer-text">© 2025 TechNova. All rights reserved.</p>
             </div>
         </footer>
     );
