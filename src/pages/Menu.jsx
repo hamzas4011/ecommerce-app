ac207016c7cb8efbe93a1c90fac9bb303a068823
Hamza Simsek <hamzas4011@hotmail.com>
2025-03-10 01:49:59 +0100

    update menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Menu.css';
 import MenuItem from '../components/MenuItem';
 
 function Menu({ addToCart }) {
     const products = [
-        { id: 1, name: 'PS5 controller', price: '$99', image: '/images/gadget-1.jpg' },
-        { id: 2, name: 'Headphones', price: '$199', image: '/images/gadget-2.jpg' },
-        { id: 3, name: 'Drone', price: '$299', image: '/images/gadget-3.jpg' },
-        { id: 4, name: 'Camera', price: '$399', image: '/images/gadget-4.jpg' },
-        { id: 5, name: 'VR glasses', price: '$499', image: '/images/gadget-5.jpg' },
-        { id: 6, name: 'Phone', price: '$599', image: '/images/gadget-6.jpg' },
+        { id: 1, name: 'PS5 controller', price: '$99', image: '/images/gadget-1.jpg', loading: "lazy" },
+        { id: 2, name: 'Headphones', price: '$199', image: '/images/gadget-2.jpg', loading: "lazy" },
+        { id: 3, name: 'Drone', price: '$299', image: '/images/gadget-3.jpg', loading: "lazy" },
+        { id: 4, name: 'Camera', price: '$399', image: '/images/gadget-4.jpg', loading: "lazy" },
+        { id: 5, name: 'VR glasses', price: '$499', image: '/images/gadget-5.jpg', loading: "lazy" },
+        { id: 6, name: 'Phone', price: '$599', image: '/images/gadget-6.jpg', loading: "lazy" },
     ];
 
     return (
@@ -22,6 +22,7 @@ function Menu({ addToCart }) {
                         name={product.name}
                         price={product.price}
                         image={product.image}
+                        loading={product.loading}
                         addToCart={() => addToCart(product)}
                     />
                 ))}
